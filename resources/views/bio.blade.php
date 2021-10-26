8a962f41fa5d5760fc5dfc396e09b2d25e398dfb
LAPTOP-5OM1FA72\Asus <60900119024@uin-alauddin.ac.id>
2021-10-26 11:54:04 +0800

    selamat pagi

---
+++ resources/views/bio.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Biodata</title>
+</head>
+<body>
+<body style="background-color:floralwhite;"> </body>
+<center>
+<font size="5px">
+	<h1>DATA DIRI</h1>
+</font>
+</center>
+	<p> Nama 					: Asfira Muhri</br>
+	<p> Nim  			: 60900119024</p>
+	<p>Kelas  	: B</p>
+    <p> email : 60900119024</p>
+</body>
+</html>
